f458c9ade88c8bc44da580392745d04315dff010
Kim min-jae <mjkim11084@gmail.com>
2026-02-16 19:42:52 +0900

    Upsolve I

--- randomDefense/0216/upsolving/I.py
+++ randomDefense/0216/upsolving/I.py
@@ -1,6 +1,14 @@
 import sys
 input=sys.stdin.readline
 n=int(input())
+arr=list(input().rstrip())
 INF=10**9
-dp=[INF]*(n+1)
+dp=[INF]*(n)
+dp[0]=0
 prev={"B":"J","O":"B","J":"O"}
+for i in range(1,n):
+    p=prev[arr[i]]
+    for j in range(i):
+        if arr[j]==p:
+            dp[i]=min(dp[i],dp[j]+(j-i)**2)
+print(-1 if dp[-1]==INF else dp[-1])
